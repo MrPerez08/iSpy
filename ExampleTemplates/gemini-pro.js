783ec01cdb889d1dc3e7cc6babbfeb75686f4129
Aidan <aidan.perez1608@gmail.com>
2024-06-29 13:03:50 -0400

    Moving examples to seperate folder for easier maintenance

--- ExampleTemplates/gemini-pro.js
+++ ExampleTemplates/gemini-pro.js
@@ -8,7 +8,7 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 async function run(){
     const model = genAI.getGenerativeModel({model: "gemini-pro"});
 
-    const prompt = "Lets meet up.";
+    const prompt = "Hey how is it going?";
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
---
+++ test.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+url = "https://discord.com/channels/@me/1237525797213044818"
+
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+webbrowser.get('chrome').open_new_tab(url)
