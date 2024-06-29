1cdb08d8460890eb50e5a0483f76346285d569c3
Aidan <aidan.perez1608@gmail.com>
2024-06-29 16:10:45 -0400

    comments

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import webbrowser
 import pyautogui
 
+#Opens Discord on the browser
 chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 url = "https://discord.com/channels/@me/1237525797213044818"
-
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 webbrowser.get('chrome').open_new_tab(url)
 
-pyautogui.moveTo(100,100)
+pyautogui.moveTo(100,100) #Instantly Moves cursor to user search
