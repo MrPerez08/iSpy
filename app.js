f6c55f373201e7a355cfecac66b46de40b6aa2be
neal joshi <???@???.com>
2024-06-29 15:42:38 -0400

    successful run

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const { spawn } = require('child_process');
 
+const childPython = spawn('python', ['test.py']);
+childPython.stdout.on('data',(data) => {
+    console.log(`stdout: ${data}`);
+});
+
+childPython.stderr.on('data',(data) => {
+    console.log(`stderr: ${data}`);
+});
+
+childPython.stdout.on('close',(code) => {
+    console.log(`child process exited with code: ${code}`);
+});
