554f213b460da23c0cc17d1aa7546cc3391189ad
Aidan <aidan.perez1608@gmail.com>
2024-06-30 00:45:49 -0400

    end of day

--- aicooksondiscord/discord_navigator.py
+++ aicooksondiscord/discord_navigator.py
@@ -2,10 +2,16 @@ import webbrowser as wb
 import pyautogui as pag
 from screeninfo import get_monitors
 import time
+import platform
+
+chrome_path =""
+match platform.system():
+    case 'Windows': chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    case 'Darwin': chrome_path = "~/Library/Application Support/Google/Chrome"
 
 #monitor 1 dimensions/resolution
 monitorWidth,monitorHeight = get_monitors()[0].width, get_monitors()[0].height
-pedo = "neal_j07"
+pedo = "neal_j07"  #<-- INSERT pedo discord usertag
 
 #Opens Discord on the browser
 chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
@@ -22,11 +28,26 @@ pag.click(20,170) #Instantly Moves cursor to add a server
 #pag.click(monitorWidth*6/10,monitorHeight*7/10)
 
 #IF IT AINT BROKE DONT FIX IT
+'''
 time.sleep(.5)
 pag.moveTo(monitorWidth * 9/10,monitorHeight/10)
 time.sleep(.5)
 pag.click(monitorWidth /2,monitorHeight/10)
 pag.leftClick(monitorWidth * 9/10,monitorHeight/10)
+pag.click(monitorWidth * 9/10,monitorHeight/6)
+pag.typewrite(pedo)
+pag.press("enter")
+pag.click(monitorWidth * 9/10,monitorHeight/6)
+pag.leftClick(monitorWidth * 9/10,monitorHeight/4)
+'''
+pag.moveTo(monitorWidth/5.5,monitorHeight/2)
+pag.scroll(15)
+while(True):
+    time.sleep(.5)
+    pag.scroll(32)
+
+
+
 
 
 
