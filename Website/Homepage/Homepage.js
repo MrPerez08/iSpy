cbe26f11b7509b06778a5004253375adba12c02d
ModernCodeMaster2022 <nuj2210@gmail.com>
2024-07-10 23:44:19 -0400

    WEBSITE GLOW UP

--- Website/Homepage/Homepage.js
+++ Website/Homepage/Homepage.js
@@ -1,1 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
 
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navbar.classList.toggle('active');
+});
