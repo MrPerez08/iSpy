0fc65709c01dbe4eeeeab68ac925ba5843c614e7
Aidan <aidan.perez1608@gmail.com>
2024-07-27 14:09:36 -0400

    lol vidhu finna lose his job

---
+++ testing/gemini/switch2.py
@@ -0,0 +1,13 @@
+from img_analyzer import *
+from video_analyzer import *
+import os
+
+filename="pic.png"
+placeholder="./testing/testing_media/"
+
+match (os.path.splitext(filename)[1]):
+    case ".png" | ".jpg":
+        print(imgimp(placeholder+ filename))
+    case ".mp4" | ".mov":
+        vidimp(placeholder + filename)
+
